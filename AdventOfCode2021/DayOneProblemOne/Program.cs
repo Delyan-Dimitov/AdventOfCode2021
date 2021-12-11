fe1b23dfc30cd6ca6d058f3bed4ab587b8c806e9
Delyan Dimitrov <delyandimitrov99@gmail.com>
2021-12-11 12:43:45 +0200

    fix

--- AdventOfCode2021/DayOneProblemOne/Program.cs
+++ AdventOfCode2021/DayOneProblemOne/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Linq;
 
-namespace Problem1
+namespace DayOneProblemOne
 {
     class Program
     {
--- AdventOfCode2021/DayOneProblemTwo/Program.cs
+++ AdventOfCode2021/DayOneProblemTwo/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Linq;
 
-namespace Problem2
+namespace DayOneProblemTwo
 {
     class Program
     {
