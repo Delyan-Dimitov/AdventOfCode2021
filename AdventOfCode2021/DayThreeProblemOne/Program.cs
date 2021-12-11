0ce97166ed34d53aff7041aebc7ea66dabd6eb5b
Delyan Dimitrov <delyandimitrov99@gmail.com>
2021-12-11 18:15:20 +0200

    add day three

---
+++ AdventOfCode2021/DayThreeProblemOne/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DayThreeProblemOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputRaw = File.ReadAllText(@"C:\Users\gunex\Desktop\AdventOfCode2021\AdventOfCode2021\Inputs\DayThree.txt");
+            string[] input = inputRaw.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            int bitsLength = 12;
+
+            char[] gamma = new char[bitsLength]; 
+            char[] epsilon = new char[bitsLength];
+
+            int[] mostCommon = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            foreach (string bits in input)
+            {
+                for (int i = 0; i < bitsLength; i++)
+                {
+                    switch (bits[i])
+                    {
+                        case '1':
+                            mostCommon[i]++;
+                            break;
+                        case '0':
+                            mostCommon[i]--;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < bitsLength; i++)
+            {
+                gamma[i] = mostCommon[i] > 0 ? '1' :'0';
+                epsilon[i] = mostCommon[i] > 0 ? '0' :'1';
+            }
+
+            int gammaRate = Convert.ToInt32(String.Join("", gamma), 2);
+            int epsilonRate = Convert.ToInt32(String.Join("", epsilon), 2);
+
+            Console.WriteLine(gammaRate * epsilonRate);
+        }
+    }
+}
---
+++ AdventOfCode2021/DayThreeProblemTwo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DayThreeProblemTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputRaw = File.ReadAllText(@"C:\Users\gunex\Desktop\AdventOfCode2021\AdventOfCode2021\Inputs\DayThree.txt");
+            string[] input = inputRaw.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            int bitsLength = input[0].Length;
+
+            string oxygenGeneratorRatingBits = GetRating(input, bitsLength, 0, '1', true);
+            string c02ScrubberRatingBits = GetRating(input, bitsLength, 0, '0', false);
+
+
+            int oxygenGeneratorRating = Convert.ToInt32(String.Join("", oxygenGeneratorRatingBits), 2);
+            int c02ScrubberRating = Convert.ToInt32(String.Join("", c02ScrubberRatingBits), 2);
+
+            Console.WriteLine(oxygenGeneratorRating * c02ScrubberRating);
+        }
+
+        private static string GetRating(string[] bitArray, int bitsLength, int position, char defaultBit, bool findMostCommon)
+        {
+            char bit;
+
+            if (bitArray.Length == 1)
+            {
+                return bitArray[0];
+            }
+
+            int bitSumAtPosition = 0;
+            foreach (string bits in bitArray)
+            {
+                switch (bits[position])
+                {
+                    case '1':
+                        bitSumAtPosition++;
+                        break;
+                    case '0':
+                        bitSumAtPosition--;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            bit = bitSumAtPosition != 0 ?
+                bitSumAtPosition < 0 ?
+                findMostCommon ? '0' : '1' :
+                findMostCommon ? '1' : '0' : defaultBit;
+                
+
+            string[] newbitArray = bitArray.Where(x => x[position] == bit).ToArray();
+            position++;
+
+            return GetRating(newbitArray, bitsLength, position, defaultBit, findMostCommon);
+        }
+    }
+}
