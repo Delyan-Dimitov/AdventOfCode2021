864524acad9ee65db444865f4ed239304a6776d3
Delyan Dimitrov <delyandimitrov99@gmail.com>
2021-12-11 16:31:00 +0200

    Add Day Two

---
+++ AdventOfCode2021/DayTwoProblemOne/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DayTwoProblemOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputRaw = File.ReadAllText(@"C:\Users\gunex\Desktop\AdventOfCode2021\AdventOfCode2021\Inputs\DayTwo.txt");
+            string[] inputs = inputRaw.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            int horizontalPostion = 0;
+            int depth = 0;
+
+            foreach (string input in inputs)
+            {
+                string[] inputSplit = input.Split(" ");
+                string command = inputSplit[0];
+                int value = int.Parse(inputSplit[1]);
+
+                switch (command)
+                {
+                    case "down":
+                        depth += value;
+                        break;
+                    case "up":
+                        depth -= value;
+                        break;
+                    case "forward":
+                        horizontalPostion += value;
+                        break;
+                    default:break;
+                }
+            }
+            Console.WriteLine(horizontalPostion * depth);
+        }
+    }
+}
---
+++ AdventOfCode2021/DayTwoProblemTwo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DayTwoProblemTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputRaw = File.ReadAllText(@"C:\Users\gunex\Desktop\AdventOfCode2021\AdventOfCode2021\Inputs\DayTwo.txt");
+            string[] inputs = inputRaw.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            int horizontalPostion = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (string input in inputs)
+            {
+                string[] inputSplit = input.Split(" ");
+                string command = inputSplit[0];
+                int value = int.Parse(inputSplit[1]);
+
+                switch (command)
+                {
+                    case "down":
+                        aim += value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                    case "forward":
+                        horizontalPostion += value;
+                        depth += aim * value;
+                        break;
+                    default: break;
+                }
+            }
+            Console.WriteLine(horizontalPostion * depth);
+        }
+    }
+}
