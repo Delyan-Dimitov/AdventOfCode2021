a6243d457e4f4d1cc436a78b6c38894e605043e6
Delyan Dimitrov <delyandimitrov99@gmail.com>
2021-12-11 12:27:14 +0200

    Add day one problems

---
+++ DayOnePartOne/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputRaw = File.ReadAllText(@"C:\Users\gunex\Desktop\AdventOfCode2021\AdventOfCode2021\Inputs\DayOnePartOne.txt");
+            int[] input = inputRaw.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+            int largerMeasurements = 0;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] > input[i - 1])
+                {
+                    largerMeasurements++;
+                }
+            }
+            Console.WriteLine(largerMeasurements);
+        }
+    }
+}
---
+++ DayOnePartTwo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputRaw = File.ReadAllText(@"C:\Users\gunex\Desktop\AdventOfCode2021\AdventOfCode2021\Inputs\DayOnePartTwo.txt");
+            int[] input = inputRaw.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            int rows = input.Length - 2;
+            int columns = 3;
+
+            int[,] groupsOfThree = new int[rows, columns];
+
+            int largerMeasurements = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                groupsOfThree[i, 0] = input[i];
+                groupsOfThree[i, 1] = input[i + 1];
+                groupsOfThree[i, 2] = input[i + 2];
+            }
+
+            for (int i = 1; i < rows; i++)
+            {
+                int currentRowsSum = groupsOfThree[i, 0] + groupsOfThree[i, 1] + groupsOfThree[i, 2];
+                int prevRowSum = groupsOfThree[i - 1, 0] + groupsOfThree[i - 1, 1] + groupsOfThree[i - 1, 2];
+
+                if (currentRowsSum > prevRowSum)
+                {
+                    largerMeasurements++;
+                }
+            }
+            Console.WriteLine(largerMeasurements);
+        }
+    }
+}
